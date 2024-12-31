c383670d4d8fac1352672f9dcc3e2fc5f15abe9a
iray <kasireddy.sriharsha@gmail.com>
2024-12-31 22:41:17 +0530

    shell starting

--- frontend.sh
+++ frontend.sh
@@ -11,7 +11,7 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp nginx.conf /etc/nginx/nginx.conf
+cp har.conf /etc/nginx/nginx.conf
 
 
 systemctl restart nginx
