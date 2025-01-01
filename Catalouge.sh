83285b6c2f45bc77aadfc0b620ab479cd3e749ff
iray <kasireddy.sriharsha@gmail.com>
2025-01-02 00:21:47 +0530

    shell starting project..

--- Catalouge.sh
+++ Catalouge.sh
@@ -8,6 +8,6 @@ app_name=catalogue
 NODEJS
 
 
-cp $dir_path/mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-mongosh -y
-mongosh --host 10.0.0.6 </app/db/master-data.js
+cp $dir_path/mongo.repo /etc/yum.repos.d/mongo.repo >$log_file
+dnf install mongodb-mongosh -y >$log_file
+mongosh --host 10.0.0.6 </app/db/master-data.js >$log_file
