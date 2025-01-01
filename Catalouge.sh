2699fee33aed36dc6eb6faadc40073d31b71a4f4
iray <kasireddy.sriharsha@gmail.com>
2025-01-01 14:12:00 +0530

    shell starting project..

--- Catalouge.sh
+++ Catalouge.sh
@@ -1,10 +1,12 @@
+cp catalogue.service /etc/systemd/system/catalogue.service
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 
-cp catalogue.service /etc/systemd/system/catalogue.service
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 useradd roboshop
 rm -rf /app
@@ -15,7 +17,8 @@ rm -f /tmp/catalogue.zip
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
 cd /app
 unzip /tmp/catalogue.zip
-
+cd /app
+npm install
 systemctl daemon-reload
 
 systemctl enable catalogue
--- MongoDB.sh
+++ MongoDB.sh
@@ -3,7 +3,7 @@ dnf install mongodb-org -y
 
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod
-systemctl start mongod
+
 
 
 
