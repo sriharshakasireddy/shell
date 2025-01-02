3c649604e29b99d6af9974522e924c3197810244
iray <kasireddy.sriharsha@gmail.com>
2025-01-02 12:54:21 +0530

    shell starting project..

--- MySQL.sh
+++ MySQL.sh
@@ -1,4 +1,13 @@
-dnf install mysql-server -y
+dnf install mysql-server -
+
+  echo enable mysqld
 systemctl enable mysqld
+  Status_Print $?
+
+  echo disbale start mysqld
 systemctl start mysqld
+  Status_Print $?
+
+  echo secure installation
 mysql_secure_installation --set-root-pass RoboShop@1
+  Status_Print $?
