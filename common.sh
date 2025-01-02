5c6b5101d87b38a8d8b258443b3737fa0e6fa456
iray <kasireddy.sriharsha@gmail.com>
2025-01-02 12:41:55 +0530

    shell starting project..

--- common.sh
+++ common.sh
@@ -118,11 +118,9 @@ systemctl restart shipping
 
 Status_Print()
 {
-  if [$? -gt 0]; then
-    echo -e "\e[31mFAILURE\e[0m"
+  if  [ $? -gt 0 ] ; then
+    echo -e "\e[38;5;214mFAILURE\e[0m"
     else
 echo -e "\e[32mSUCCESS\e[0m"
-exit
-
 fi
 }
