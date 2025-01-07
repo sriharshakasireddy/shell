77d927b758ea41761d21149a7ef1387a38046c86
iray3 <kasireddy.sriharsha@gmail.com>
2025-01-08 00:10:47 +0530

    s

--- Catalouge.sh
+++ Catalouge.sh
@@ -10,4 +10,4 @@ NODEJS
 
 cp $dir_path/mongo.repo /etc/yum.repos.d/mongo.repo >$log_file
 dnf install mongodb-mongosh -y >$log_file
-mongosh --host 10.0.0.6 </app/db/master-data.js >$log_file
+mongosh --host 10.0.0.8 </app/db/master-data.js >$log_file
